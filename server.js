87903f9b4e47ba54b87cb98f2051fd0da45c64cc
guuroh christina <guu>
2026-05-12 16:07:54 +0000

    update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post('/send-sms', async (req, res) => {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        recipient: ["0551218719"],
+        recipient: ["0507585070"],
         sender: "Loco Loca", // Use "mNotify" as sender until "LocoLoca" is approved by them
         message: message,
         is_schedule: false
